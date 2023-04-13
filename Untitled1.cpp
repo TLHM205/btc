6a4d9a973a10224a7a61e5c22f688e59b5463daf
TLHM205 <117747723+TLHM205@users.noreply.github.com>
2023-04-13 09:39:11 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h> 
+int main(){
+	/*
+	int a,b,c;
+	printf("Nhap a va b: ");
+	scanf("%d %d",&a,&b);
+		c = a + b;
+	printf("%d\n",c);
+		c = a- b;
+	printf("%d\n",c);
+		c = a*b;
+	printf("%d\n",c);
+		c = a/b;
+	printf("%d\n",c);
+	
+	
+	int c;
+	printf("Nhap c: ");
+	scanf("%d",&c);
+	printf("\nputch c = "); putch(c);
+
+	// Tinh dien tich hinh cn 
+	float a,b,dt,cv;
+	printf("Nhap do dai canh a: ");
+	scanf("%f",&a);
+	printf("Nhap do dai cach b: ");
+	scanf("%f",&b);
+	dt =a*b;
+	cv = 2*(a+b);
+	printf("Dien tich hinh cn la: %5.2f",dt);
+	printf("\n");
+	printf("Chu vi hinh chu nhat la: %5.2f",cv);
+	
+	float a,b,c,p,s,q;
+	printf("Nhap a,b,c: ");
+	scanf("%f %f %f",&a,&b,&c);
+		p = a+b+c;
+		q = p/2; 
+		s = sqrt(q*(q-a) *(q-b)*(q-c));
+		printf("Chu vi cua tam giac %5.2f, Dien tich cua tam giac %5.2f",s,p);
+		*/
+		
+		
+		}
